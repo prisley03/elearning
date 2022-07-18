9512b554cfe81ec857a5d45e55d42d13abc05965
Aradhana Indra D(ev) <aradhanaindradev@gmail.com>
2022-07-18 10:11:51 +0700

    Added First CSS

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/global.css">
     <link rel="stylesheet" href="/assets/css/style.css">
     <title>E-Learning</title>
 </head>
@@ -17,62 +18,72 @@
         <!-- Navbar -->
     </nav>
     <main>
-        <h1>Recently Added</h1>
-        <section class="cards">
 
+        <section class="cards">
+            <p class="title">Recently Added</p>
         </section>
 
-        <h1>Available Degrees</h1>
         <section class="degrees">
+            <p class="title">Available Degrees</p>
             <div class="cards">
-
-            </div>
-            <div class="cards">
-                <img src="" alt="">
+                <img src="/assets/img/icons/accounting-white.svg" alt="">
                 <p>Accounting</p>
             </div>
             <div class="cards">
-                <img src="" alt="">
+                <img src="/assets/img/icons/chineseliterature-white.svg" alt="">
                 <p>Chinese Literature</p>
             </div>
             <div class="cards">
-                <img src="" alt="">
+                <img src="/assets/img/icons/computerscience-white.svg" alt="">
                 <p>Computer Science</p>
             </div>
             <div class="cards">
-                <img src="" alt="">
+                <img src="/assets/img/icons/entrepreneurship-white.svg" alt="">
                 <p>Entrepreneurship</p>
             </div>
             <div class="cards">
-                <img src="" alt="">
+                <img src="/assets/img/icons/foodtechnology-white.svg" alt="">
                 <p>Food Technology</p>
             </div>
             <div class="cards">
-                <img src="" alt="">
+                <img src="/assets/img/icons/informationsystems-white.svg" alt="">
                 <p>Information Systems</p>
             </div>
             <div class="cards">
-                <img src="" alt="">
-                <p>Information Systems</p>
-            </div>
-            <div class="cards">
-                <img src="" alt="">
+                <img src="/assets/img/icons/industrialengineering-white.svg" alt="">
                 <p>Industrial Engineering</p>
             </div>
-            <div class="cards"></div>
-            <div class="cards"></div>
-            <div class="cards"></div>
+            <div class="cards">
+                <img src="/assets/img/icons/ibm-white.svg" alt="">
+                <p>International Business Management</p>
+            </div>
+            <div class="cards">
+                <img src="/assets/img/icons/management-white.svg" alt="">
+                <p>Management</p>
+            </div>
+            <div class="cards">
+                <img src="/assets/img/icons/psychology-white.svg" alt="">
+                <p>Psychology</p>
+            </div>
+            <div class="cards">
+                <img src="/assets/img/icons/ISM-white.svg" alt="">
+                <p>Information System & Management</p>
+            </div>
+            <div class="cards">
+                <img src="/assets/img/icons/ISA-white.svg" alt="">
+                <p>Information System & Accounting</p>
+            </div>
         </section>
 
         <section class="info">
             <div class="contribute">
-                <h1>Want to contribute?</h1>
+                <p class="title">Want to contribute?</p>
             </div>
             <div class="menteeRegistration">
-                <h1>Become a Mentee</h1>
+                <p class="title">Become a Mentee</p>
             </div>
             <div class="contact">
-                <h1>Contact Us</h1>
+                <p class="title">Contact Us</p>
             </div>
         </section>
     </main>
