0e62de0da43f63fbb915fd343ea4c7262c1c7bc0
Aradhana Indra D(ev) <aradhanaindradev@gmail.com>
2022-07-18 11:14:57 +0700

    Added Cards

--- index.php
+++ index.php
@@ -19,68 +19,100 @@
     </nav>
     <main>
 
-        <section class="cards">
+        <section class="recentlyadded">
             <p class="title">Recently Added</p>
         </section>
 
         <section class="degrees">
             <p class="title">Available Degrees</p>
-            <div class="cards">
-                <img src="/assets/img/icons/accounting-white.svg" alt="">
-                <p>Accounting</p>
+            <div class="cards-container">
+                <div class="cards">
+                    <img src="/assets/img/icons/accounting-white.svg" alt="">
+                    <p>Accounting</p>
+                    <div class="hue accounting"></div>
+                </div>
+                <div class="cards">
+                    <img src="/assets/img/icons/chineseliterature-white.svg" alt="">
+                    <p>Chinese Literature</p>
+                    <div class="hue chineseliterature"></div>
+                </div>
+                <div class="cards">
+                    <img src="/assets/img/icons/computerscience-white.svg" alt="">
+                    <p>Computer Science</p>
+                    <div class="hue computerscience"></div>
+                </div>
+                <div class="cards">
+                    <img src="/assets/img/icons/entrepreneurship-white.svg" alt="">
+                    <p>Entrepreneurship</p>
+                    <div class="hue entrepreneurship"></div>
+                </div>
+                <div class="cards">
+                    <img src="/assets/img/icons/foodtechnology-white.svg" alt="">
+                    <p>Food Technology</p>
+                    <div class="hue foodtechnology"></div>
+                </div>
+                <div class="cards">
+                    <img src="/assets/img/icons/informationsystems-white.svg" alt="">
+                    <p>Information Systems</p>
+                    <div class="hue informationsystems"></div>
+                </div>
+                <div class="cards">
+                    <img src="/assets/img/icons/industrialengineering-white.svg" alt="">
+                    <p>Industrial Engineering</p>
+                    <div class="hue industrialengineering"></div>
+                </div>
+                <div class="cards">
+                    <img src="/assets/img/icons/ibm-white.svg" alt="">
+                    <p>International Business Management</p>
+                    <div class="hue ibm"></div>
+                </div>
+                <div class="cards">
+                    <img src="/assets/img/icons/management-white.svg" alt="">
+                    <p>Management</p>
+                    <div class="hue management"></div>
+                </div>
+                <div class="cards">
+                    <img src="/assets/img/icons/psychology-white.svg" alt="">
+                    <p>Psychology</p>
+                    <div class="hue psychology"></div>
+                </div>
+                <div class="cards">
+                    <img src="/assets/img/icons/ISM-white.svg" alt="">
+                    <p>Information System & Management</p>
+                    <div class="hue ism"></div>
+                </div>
+                <div class="cards">
+                    <img src="/assets/img/icons/ISA-white.svg" alt="">
+                    <p>Information System & Accounting</p>
+                    <div class="hue isa"></div>
+                </div>
             </div>
-            <div class="cards">
-                <img src="/assets/img/icons/chineseliterature-white.svg" alt="">
-                <p>Chinese Literature</p>
-            </div>
-            <div class="cards">
-                <img src="/assets/img/icons/computerscience-white.svg" alt="">
-                <p>Computer Science</p>
-            </div>
-            <div class="cards">
-                <img src="/assets/img/icons/entrepreneurship-white.svg" alt="">
-                <p>Entrepreneurship</p>
-            </div>
-            <div class="cards">
-                <img src="/assets/img/icons/foodtechnology-white.svg" alt="">
-                <p>Food Technology</p>
-            </div>
-            <div class="cards">
-                <img src="/assets/img/icons/informationsystems-white.svg" alt="">
-                <p>Information Systems</p>
-            </div>
-            <div class="cards">
-                <img src="/assets/img/icons/industrialengineering-white.svg" alt="">
-                <p>Industrial Engineering</p>
-            </div>
-            <div class="cards">
-                <img src="/assets/img/icons/ibm-white.svg" alt="">
-                <p>International Business Management</p>
-            </div>
-            <div class="cards">
-                <img src="/assets/img/icons/management-white.svg" alt="">
-                <p>Management</p>
-            </div>
-            <div class="cards">
-                <img src="/assets/img/icons/psychology-white.svg" alt="">
-                <p>Psychology</p>
-            </div>
-            <div class="cards">
-                <img src="/assets/img/icons/ISM-white.svg" alt="">
-                <p>Information System & Management</p>
-            </div>
-            <div class="cards">
-                <img src="/assets/img/icons/ISA-white.svg" alt="">
-                <p>Information System & Accounting</p>
-            </div>
+
         </section>
 
         <section class="info">
             <div class="contribute">
                 <p class="title">Want to contribute?</p>
+                <ol type="1">
+                    <li>
+                        Create a module with Binus Student Learning Community's template.
+                    </li>
+                    <li>
+                        Fill this module submission form.
+                    </li>
+                    <li>
+                        Your module will be added within 1 - 7 days.
+                    </li>
+                </ol>
             </div>
             <div class="menteeRegistration">
                 <p class="title">Become a Mentee</p>
+                <p>
+                    Get exclusive modules and learn together alongside a verified mentor and other mentees!
+                </p>
+                <div class="registrationButton">
+                    <p>Register</p>
+                </div>
             </div>
             <div class="contact">
                 <p class="title">Contact Us</p>
